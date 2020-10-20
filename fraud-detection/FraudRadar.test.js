ee2717112a74ef30e575d8cbebb353d09d60a63a
lrevilla <lrevilla@solidgear.es>
2020-10-20 18:53:46 +0200

    Add a couple of tests fo ensure the orderIds

--- fraud-detection/FraudRadar.test.js
+++ fraud-detection/FraudRadar.test.js
@@ -1,15 +1,22 @@
 const FraudRadar = require('./FraudRadar')
 const assert = require('assert')
 
+const ORDER_FILE_NAMES = {
+  ONE_LINE: 'OneLineFile.txt',
+  TWO_LINES: 'TwoLines_FraudulentSecond.txt',
+  THREE_LINES: 'ThreeLines_FraudulentSecond.txt',
+  FOUR_LINES: 'FourLines_MoreThanOneFraudulent.txt'
+}
+
 describe('Fraud Radar', function () {
   it('Should process the one line file', function () {
-    const result = FraudRadar.Check('OneLineFile.txt')
+    const result = FraudRadar.Check(ORDER_FILE_NAMES.ONE_LINE)
     assert.ok(result)
     assert.strictEqual(result.length, 0)
   })
 
   it('Should process the two line file in which the second is fraudulent', function () {
-    const result = FraudRadar.Check('TwoLines_FraudulentSecond.txt')
+    const result = FraudRadar.Check(ORDER_FILE_NAMES.TWO_LINES)
     assert.ok(result)
     assert.strictEqual(result.length, 1)
     assert.strictEqual(result[0].isFraudulent, true)
@@ -17,7 +24,7 @@ describe('Fraud Radar', function () {
   })
 
   it('Should process the three line file in which the second is fraudulent', function () {
-    const result = FraudRadar.Check('ThreeLines_FraudulentSecond.txt')
+    const result = FraudRadar.Check(ORDER_FILE_NAMES.THREE_LINES)
     assert.ok(result)
     assert.strictEqual(result.length, 1)
     assert.strictEqual(result[0].isFraudulent, true)
@@ -25,8 +32,13 @@ describe('Fraud Radar', function () {
   })
 
   it('Should process the four line file in which more than one order is fraudulent', function () {
-    const result = FraudRadar.Check('FourLines_MoreThanOneFraudulent.txt')
+    const result = FraudRadar.Check(ORDER_FILE_NAMES.FOUR_LINES)
     assert.ok(result)
     assert.strictEqual(result.length, 2)
+    const fraudulentOrderIds = [2, 4]
+    result.forEach((order, index) => {
+      assert.strictEqual(order.isFraudulent, true)
+      assert.strictEqual(order.orderId, fraudulentOrderIds[index])
+    })
   })
 })
