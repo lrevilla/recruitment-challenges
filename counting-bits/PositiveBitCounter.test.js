18a04f8c62253c2a45fcbe739f8fccba6789b037
lrevilla <lrevilla@solidgear.es>
2020-10-24 17:50:55 +0200

    move tests from deepEqual to deepStrictEqual since deepEqual is deprecated

--- counting-bits/PositiveBitCounter.test.js
+++ counting-bits/PositiveBitCounter.test.js
@@ -3,28 +3,25 @@ const assert = require('assert')
 
 describe('PositiveBitCounter', function () {
   it('Should return a RangeError when a negative value is supplied', function () {
-    assert.throws(
-      () => PositiveBitCounter.Count(-2),
-      function isRangeError (error) {
-        return (error instanceof RangeError)
-      })
+    assert.throws(() => PositiveBitCounter.Count(-2),
+      function isRangeError(error) { return (error instanceof RangeError) })
   })
 
   it('Should return zero occurrences for input = 0', function () {
     const expected = [0]
     const actual = PositiveBitCounter.Count(0)
-    assert.deepEqual(actual, expected)
+    assert.deepStrictEqual(actual, expected)
   })
 
   it('Should return the expected count for input = 1', function () {
     const expected = [1, 0]
     const actual = PositiveBitCounter.Count(1)
-    assert.deepEqual(actual, expected)
+    assert.deepStrictEqual(actual, expected)
   })
 
   it('Should return the expected count for input = 161', function () {
     const expected = [3, 0, 5, 7]
     const actual = PositiveBitCounter.Count(161)
-    assert.deepEqual(actual, expected)
+    assert.deepStrictEqual(actual, expected)
   })
 })
