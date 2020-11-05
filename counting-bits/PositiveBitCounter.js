69668e141b7cb74640d285bfead66d23d08a1c36
lrevilla <lrevilla@solidgear.es>
2020-11-05 15:09:09 +0100

    Fix typo on 8 bits grouping (0x0F0F0F0F to 0x00FF00FF)

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -2,7 +2,7 @@ const BINARY_MASKS = {
   GROUP_BY_1_BIT: { dec: 1, hex: 0x55555555 }, // 1010101010101010... in hexadecimal
   GROUP_BY_2_BITS: { dec: 2, hex: 0x33333333 }, // 1100110011001100... in hexadecimal
   GROUP_BY_4_BITS: { dec: 4, hex: 0x0F0F0F0F }, // 1111000011110000... in hexadecimal
-  GROUP_BY_8_BITS: { dec: 8, hex: 0x0F0F0F0F }, // 1111111100000000... in hexadecimal
+  GROUP_BY_8_BITS: { dec: 8, hex: 0x00FF00FF }, // 1111111100000000... in hexadecimal
   GROUP_BY_16_BITS: { dec: 16, hex: 0x0000FFFF } // 1111111111111111... in hexadecimal
 }
 
@@ -22,12 +22,12 @@ exports.Count = input => {
   const integerInput = BigInt(input, 10)
   if (integerInput < 0 || integerInput === Infinity) throw new RangeError()
 
-  let bitCount = input - ((input >> 1) & BINARY_MASKS.GROUP_BY_1_BIT.hex)
-  bitCount = ((bitCount >> BINARY_MASKS.GROUP_BY_2_BITS.dec) & BINARY_MASKS.GROUP_BY_2_BITS.hex) +
-    (bitCount & BINARY_MASKS.GROUP_BY_2_BITS.hex)
-  bitCount = ((bitCount >> BINARY_MASKS.GROUP_BY_4_BITS.dec) + bitCount) & BINARY_MASKS.GROUP_BY_4_BITS.hex
-  bitCount = ((bitCount >> BINARY_MASKS.GROUP_BY_8_BITS.dec) + bitCount) & BINARY_MASKS.GROUP_BY_8_BITS.hex
-  bitCount = ((bitCount >> BINARY_MASKS.GROUP_BY_16_BITS.dec) + bitCount) & BINARY_MASKS.GROUP_BY_16_BITS.hex
+  let value = input - ((input >> BINARY_MASKS.GROUP_BY_1_BIT.dec) & BINARY_MASKS.GROUP_BY_1_BIT.hex)
+  value = ((value >> BINARY_MASKS.GROUP_BY_2_BITS.dec) & BINARY_MASKS.GROUP_BY_2_BITS.hex) +
+    (value & BINARY_MASKS.GROUP_BY_2_BITS.hex)
+  value = ((value >> BINARY_MASKS.GROUP_BY_4_BITS.dec) + value) & BINARY_MASKS.GROUP_BY_4_BITS.hex
+  value = ((value >> BINARY_MASKS.GROUP_BY_8_BITS.dec) + value) & BINARY_MASKS.GROUP_BY_8_BITS.hex
+  const bitCount = ((value >> BINARY_MASKS.GROUP_BY_16_BITS.dec) + value) & BINARY_MASKS.GROUP_BY_16_BITS.hex
 
   const indexes = Array.from({ length: bitCount })
     .map((item, index) => findIndexOfBitAtPossition(input, index + 1))
